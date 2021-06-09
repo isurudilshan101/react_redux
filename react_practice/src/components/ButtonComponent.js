cf43591557eba2bf3ce6b634f6ff5b5b2782c14b
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2021-06-09 14:05:12 +0530

    redux

--- react_practice/src/components/ButtonComponent.js
+++ react_practice/src/components/ButtonComponent.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {fetchPost} from '../actions/postAction';
+import {bindActionCreator} from 'redux';
+import {connect} from 'react-redux';
 
 export default class ButtonComponent extends Component {
   render() {
